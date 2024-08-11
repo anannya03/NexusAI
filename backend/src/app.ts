5d0289431bb7c3b73d84c6d96abaae7b13ef4bc9
Anannya Patra <40665106+anannya03@users.noreply.github.com>
2024-08-11 18:33:15 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
