803144c16d05596485d111e38f0171e99ca006f0
anannya03 <aaniee09@gmail.com>
2024-08-11 18:24:39 +0530

    Hopeful

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "https://nexus-ai-frontend.vercel.app/", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
