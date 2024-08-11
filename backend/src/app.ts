4c6c098b3354ef095c4e722bd19482a83520851b
anannya03 <aaniee09@gmail.com>
2024-08-11 18:07:29 +0530

    changed domains

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://nexus-ai-frontend.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
